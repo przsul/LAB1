61bd9707c9770ff0c01bb7a66c0b7c3edc652569
Przemyslaw Sulecki <przemek.sulecki.mail@gmail.com>
2019-06-12 21:51:43 +0200

    Exercise 3

---
+++ exercise3.py
@@ -0,0 +1,16 @@
+# Exercise 3
+
+A = set(input("Enter first set: "))
+B = set(input("Enter second set: "))
+
+# union 
+print("Union :", A | B) 
+  
+# intersection 
+print("Intersection :", A & B) 
+  
+# difference 
+print("Difference :", A - B) 
+  
+# symmetric difference 
+print("Symmetric difference :", A ^ B) 
