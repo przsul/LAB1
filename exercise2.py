bbf967716b48e489591b2d666f9bc6d62172bc1b
Przemyslaw Sulecki <przemek.sulecki.mail@gmail.com>
2019-06-17 13:00:25 +0200

    Fix exercise2

--- exercise2.py
+++ exercise2.py
@@ -1,4 +1,16 @@
 # Exercise 2
 
-binary = raw_input("Enter binary string: ").count('1')
-print "Number of gaps: " + str(binary-1)
+binary = raw_input("Enter binary string: ")
+ones = binary.count('1')
+tmp = 0
+
+i = 0
+while i < len(binary):
+  if i == len(binary)-1:
+    i += 1
+    continue
+  if binary[i] == '1' and binary[i+1] == '1':
+    tmp += 1
+  i += 1
+
+print "Number of gaps: " + str(ones-1-tmp)
